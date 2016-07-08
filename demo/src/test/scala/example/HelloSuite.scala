91f4d4e225354b636532245d1b0c4a19fc97f851
Luke Sosnicki <luke.sosnicki@me.com>
2016-07-08 20:17:32 +0200

    Example project added

--- demo/src/test/scala/example/HelloSuite.scala
+++ demo/src/test/scala/example/HelloSuite.scala
@@ -5,7 +5,7 @@ import org.scalatest.FunSuite
 /**
   * Created by sosnickl on 04.07.2016.
   */
-class HelloTest extends FunSuite {
+class HelloSuite extends FunSuite {
   test("sayHello method works correctly") {
     val hello = new Hello
     assert(hello.sayHello("Scala") == "Hello Scala!")
