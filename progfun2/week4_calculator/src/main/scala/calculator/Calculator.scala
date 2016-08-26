f972caf54b1090fc9566a855f9203deb8ddb5715
Luke Sosnicki <luke.sosnicki@me.com>
2016-08-26 14:23:50 +0200

    progfun2 week 4 assignment

---
+++ progfun2/week4_calculator/src/main/scala/calculator/Calculator.scala
@@ -0,0 +1,47 @@
+package calculator
+
+sealed abstract class Expr
+
+final case class Literal(v: Double) extends Expr
+
+final case class Ref(name: String) extends Expr
+
+final case class Plus(a: Expr, b: Expr) extends Expr
+
+final case class Minus(a: Expr, b: Expr) extends Expr
+
+final case class Times(a: Expr, b: Expr) extends Expr
+
+final case class Divide(a: Expr, b: Expr) extends Expr
+
+object Calculator {
+
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    namedExpressions.map { case (name, expr) => name -> Var(eval(expr(), namedExpressions - name)) }
+  }
+
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
+    expr match {
+      case e: Literal => e.v
+      case e: Ref => eval(getReferenceExpr(e.name, references), references)
+      case e: Plus => eval(e.a, references) + eval(e.b, references)
+      case e: Minus => eval(e.a, references) - eval(e.b, references)
+      case e: Times => eval(e.a, references) * eval(e.b, references)
+      case e: Divide => eval(e.a, references) / eval(e.b, references)
+      case _ => Double.NaN
+    }
+  }
+
+  /** Get the Expr for a referenced variables.
+    * If the variable is not known, returns a literal NaN.
+    */
+  private def getReferenceExpr(name: String,
+                               references: Map[String, Signal[Expr]]) = {
+    references.get(name).fold[Expr] {
+      Literal(Double.NaN)
+    } { exprSignal =>
+      exprSignal()
+    }
+  }
+
+}
